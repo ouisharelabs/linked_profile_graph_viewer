2869da894cec7f23c0db104f94868bb57a2ec87b
Jon Richter <post@almereyda.de>
2014-05-30 23:25:53 +0200

    Remove Django.

--- setupdb.sql
+++
@@ -1,4 +0,0 @@
-drop database if exists psi_db;
-create database psi_db;
-grant usage on *.* to psi_admin@localhost identified by 'BerlinParis';
-grant all privileges on psi_db.* to psi_admin@localhost;
